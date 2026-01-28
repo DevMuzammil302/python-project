5bd5859884b9a58fd127894800c24eb11cb13a88
Muhammad Muzammil Baig <devmuzammil302@gmail.com>
2026-01-28 16:42:21 +0500

    First commit

---
+++ guess_the_number_game.py
@@ -0,0 +1,19 @@
+import random
+
+# computer ka number
+secret_number = random.randint(1, 10)
+
+attempts = 0
+
+while True:
+    guess = int(input("Guess a number between 1 and 10: "))
+    attempts += 1
+
+    if guess == secret_number:
+        print("🎉 Correct! You guessed the number.")
+        print("Total attempts:", attempts)
+        break
+    elif guess < secret_number:
+        print("Too low! Try again.")
+    else:
+        print("Too high! Try again.")
